80de85dffa947018124b2f0e2c30c1885b76ffb3
Kuzmrom7 <kuzmrom7@gmail.com>
2018-03-14 15:27:04 +0300

    add method for orders

--- api/models/order.js
+++ api/models/order.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
+  date : mongoose.Schema.Types.Date,
   id_place: String,
   id_address: String,
   name_user: String,
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -2,23 +2,63 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const logger = require('../../logger/logger');
-const {Io} = require('../../server');
-// const socket = server.socket;
+
+
 
 const Order = require('../models/order');
 
-router.get('/', (req, res, next) => {
 
-  res.status(200).json({
-    message: 'Orders were fetched'
-  });
+
+// METHOD GET ORDERS FOR PLACE
+router.get('/place/:id_place', (req, res, next) => {
+
+  Order.find({id_place : req.params.id_place})
+    .exec()
+    .then((result) => {
+      res.status(200).json(result)
+    });
+
 });
+
+// METHOD GET ORDERS FOR ADDRESS
+router.get('/address/:id_address', (req, res, next) => {
+
+  Order.find({id_address : req.params.id_address})
+    .exec()
+    .then((result) => {
+      res.status(200).json(result)
+    });
+
+});
+
+
+// METHOD GET ORDERS FOR ORDER_ID
+
+router.get ('/:id', (req,res) => {
+  const id = req.params.id;
+
+  Order.findById(id)
+
+    .exec()
+    .then ( result => {
+      res.status(200).json(result)
+    })
+    .catch( error => {
+      res.status(400).json({
+        error : error
+      })
+    })
+});
+
+
+//CREATE ORDER AND CONNECT TO WSS
 
 module.exports =  function(io) {
   router.post('/', (req, res, next) => {
 
     const order = new Order({
       _id: new mongoose.Types.ObjectId(),
+      date : req.body.date,
       id_place: req.body.id_place,
       id_address: req.body.id_address,
       name_user: req.body.name_user,
